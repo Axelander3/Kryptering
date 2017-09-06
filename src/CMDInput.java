eeb75a57ff034873b4af8680460302d39793390a
Axelander3 <te15.alexander.mattsson@umea.nti.se>
2017-09-06 12:54:10 +0200

    Börjar med GUI och ny input.txt fil till input.

--- src/CMDInput.java
+++ src/CMDInput.java
@@ -1,5 +1,14 @@
+import java.io.*;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 /**
- * Created by alexander on 2017-08-30.
+ * Author: Alexander
+ *
+ * Last uppdate: ons 6 september 2017
+ *
  */
 public class CMDInput {
 
@@ -8,18 +17,42 @@ public class CMDInput {
 
         // CMDInput args1 args2 args3
 
-        if (args.length > 3) {
-            System.out.println("Too many arguments");
-        } else if (args.length == 3) {
-            System.out.println(args[2] + args[1] + args[0]);
-        } else if (args.length == 2) {
-            try {
-                int sum =  Integer.parseInt(args[0])+Integer.parseInt(args[1]);
-                System.out.println(args[0] + " + " + args[1] + " = " + sum);
-            } catch (NumberFormatException e) {
+
+
+        List<String> listInput = new ArrayList<>();
+
+        try {
+            Scanner sc = new Scanner(new File(args[0]));
+
+            while(sc.hasNextLine()) {
+                listInput.add(sc.nextLine());
             }
-        } else if (args.length == 1) {
-            System.out.println("You say: " + args[0]);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        String[] input = new String[listInput.size()];
+
+        input = listInput.toArray(input);
+
+
+        if (input.length > 3) {
+            System.out.println("Too many arguments");
+            for (String s : input) {
+                System.out.println(s);
+            }
+        } else if (input.length == 3) {
+            System.out.println(input[2] + " " + input[1]  + " " + input[0]);
+        } else if (input.length == 2) {
+            try {
+                int sum =  Integer.parseInt(input[0])+Integer.parseInt(input[1]);
+                System.out.println(input[0] + " + " + input[1] + " = " + sum);
+            } catch (NumberFormatException e) {
+                System.out.println("Both arguments is not numbers.");
+            }
+        } else if (input.length == 1) {
+            System.out.println("You say: " + input[0]);
 
         } else {
            System.out.println("No argmuments specified");
---
+++ src/GUI.java
@@ -0,0 +1,7 @@
+/**
+ * Created by alexander on 2017-09-06.
+ */
+public class GUI {
+
+
+}
